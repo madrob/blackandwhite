ff74247c2546a0e5ace0c15b3f471077dc8b9e51
mike <mdrob@mdrob.com>
2015-10-07 13:38:34 -0500

    Add Medium Difficulty
    Added medium difficulty which attempts to avoid overbidding when
    possible. Still pretty bad though.

--- game.js
+++ game.js
@@ -1,23 +1,60 @@
 /* vim: set ts=4 sw=4 tw=4 et : */
 
+// Generate a random integer between min and max (inclusive)
+function randInt(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+var easy = {
+    play: function() {
+        return randInt(0, state.pb);
+    },
+    record: function() { },
+    name: 'random'
+}
+var medium = {
+    play: function() {
+        var wager = randInt(0, state.pb);
+        if ($('#aflag').hasClass('white')) {
+            wager = Math.min(wager, 10);
+        } else if ($('#aflag').hasClass('black')) {
+           if (wager < 10) wager = 0;
+        } else {
+            // We're first
+            wager = Math.min(wager, 99 - (20 * $('.a').filter('.unlit').length))
+        }
+
+        return wager;
+    },
+    record: function() { },
+    name: 'medium'
+}
+var hard = {
+    play: function() {
+        return 0;
+    },
+    record: function() { },
+    name: 'hard'
+}
+
+function select(name) {
+    if (name == 'easy') return easy;
+    if (name == 'medium') return medium;
+    if (name == 'hard') return hard;
+    return medium;
+}
+
 function newState() {
     return {
         pa: 99,
         wa: 0,
         pb: 99,
         wb: 0,
-        human: (Math.random() > 0.5) // Random starting player
+        human: (Math.random() > 0.5), // Random starting player
+        ai: (select($('input[name=diff]:checked').val()))
     }
 }
 var state = newState();
-
-var easy = {
-    play: function() {
-        return Math.random() * state.pb;
-    },
-    record: function() {
-    }
-}
 
 function restart() {
     state = newState();
@@ -26,15 +63,13 @@ function restart() {
 	$('#wager').spinner('value', 0).spinner('option', 'max', 99);
     $('#sa, #sb').text('0');
     $('#round').text('1');
+	$('#submit').button('enable').button('option', 'label', 'Submit!').off('click').click(submit);
 }
 function increment(what) {
 	const current = parseInt(what.text());
 	what.text(current + 1);
 }
 
-function aiRecord() {
-    easy.record();
-}
 function evaluate() {
 	if (state.wa > state.wb) {
         increment($('#sa'));
@@ -52,14 +87,14 @@ function evaluate() {
 	}
 
 	state.wa = state.wb = 0;
-	aiRecord();
+	state.ai.record();
 }
 function setFlag(wager, flag) {
 	if (wager < 10) flag.addClass('white');
 	else flag.addClass('black');
 }
 function aiPlay() {
-	state.wb = easy.play();
+	state.wb = state.ai.play();
 	setFlag(state.wb, $('#bflag'));
 	state.pb = state.pb - state.wb;
 	if (state.pb < 80) $('#b80').addClass('unlit');
@@ -118,7 +153,7 @@ $(function() {
         heightStyle: 'content',
         active: false
     });
-    $('#difficulty').buttonset();
+    $('#diff').buttonset();
 	if (!state.human) {
 		aiPlay();
 	}
